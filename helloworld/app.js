0eb126ea0e09e8ad92c3d9a2364b7d7c9c03bf9f
Mark Mandel <markmandel@google.com>
2015-10-03 15:19:46 -0700

    Simple logging and stuff.

--- helloworld/app.js
+++ helloworld/app.js
@@ -3,10 +3,13 @@
  */
 
 var express = require("express");
+var morgan = require('morgan');
+
 var app = express();
+app.use(morgan('combined'));
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    res.send('{"foo": "bar"}');
 });
 
 var server = app.listen(8080, function () {
