7b690a95b48789c20919056fbca0f7ef7bd9f8d3
Mark Mandel <markmandel@google.com>
2015-10-07 13:26:46 -0700

    Simple node app, with Jade!

---
+++ src/sukie/app.js
@@ -0,0 +1,26 @@
+/**
+ * Simple helloworld application
+ */
+
+var express = require("express");
+var morgan = require('morgan');
+
+var app = express();
+app.set("view engine", "jade");
+
+app.use(morgan('combined'));
+
+
+app.get('/', function(req, res) {
+    res.render("index");
+});
+
+//get the port
+var port = process.env.PORT || 8080;
+
+var server = app.listen(port, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Sukie! started: http://%s:%s', host, port);
+});
